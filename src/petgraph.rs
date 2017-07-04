f978ef93f0337793a0f70ef0217dda7b9fb3b61e
BurntPizza <xburntpizza@gmail.com>
2017-07-04 19:21:10 -0400

    Use macro to DRY petgraph implementations

--- src/petgraph.rs
+++ src/petgraph.rs
@@ -1,31 +1,42 @@
 
+use pg::Incoming;
+use pg::prelude::NodeIndex;
+use pg::{graph, stable_graph};
+
 use ::*;
 
-impl<'a, N, E> Graph<pg::prelude::NodeIndex> for &'a pg::stable_graph::StableDiGraph<N, E> {
-    type Neighbors = pg::stable_graph::Neighbors<'a, E>;
+macro_rules! graph_impl {
+    ($($t:tt),*; $ty:ty, $neighbors:ty) => {
+        impl<'a, $($t),*> Graph<NodeIndex> for &'a $ty {
+            type Neighbors = $neighbors;
 
-    fn entry(&self) -> pg::prelude::NodeIndex {
-        self.node_indices()
-            .filter(|i| {
-                self.neighbors_directed(*i, pg::Direction::Incoming).count() == 0
-            })
-            .next()
-            .unwrap()
-    }
+            fn entry(&self) -> NodeIndex {
+                self.node_indices()
+                    .filter(|i| {
+                        self.neighbors_directed(*i, Incoming).count() == 0
+                    })
+                    .next()
+                    .unwrap()
+            }
 
-    fn exit(&self) -> pg::prelude::NodeIndex {
-        self.node_indices()
-            .rev()
-            .filter(|i| self.neighbors(*i).count() == 0)
-            .next()
-            .unwrap()
-    }
+            fn exit(&self) -> NodeIndex {
+                self.node_indices()
+                    .rev()
+                    .filter(|i| self.neighbors(*i).count() == 0)
+                    .next()
+                    .unwrap()
+            }
 
-    fn immediate_predecessors(&self, node: pg::prelude::NodeIndex) -> Self::Neighbors {
-        self.neighbors_directed(node, pg::Incoming)
-    }
+            fn immediate_predecessors(&self, node: NodeIndex) -> Self::Neighbors {
+                self.neighbors_directed(node, Incoming)
+            }
 
-    fn immediate_successors(&self, node: pg::prelude::NodeIndex) -> Self::Neighbors {
-        self.neighbors(node)
-    }
+            fn immediate_successors(&self, node: NodeIndex) -> Self::Neighbors {
+                self.neighbors(node)
+            }
+        }
+    };
 }
+
+graph_impl!(N, E; graph::DiGraph<N, E>, graph::Neighbors<'a, E>);
+graph_impl!(N, E; stable_graph::StableDiGraph<N, E>, stable_graph::Neighbors<'a, E>);
